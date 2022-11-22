aa8aefad40eb8f378a1db042e186d5132157ded1
Carlos Perez <pcarlos132@yahoo.com>
2022-11-22 11:28:19 -0600

    added roll button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import Die from "./Die"
+import {nanoid} from "nanoid"
 
 function App() {
+
+const [dice, setDice] = React.useState(allNewDice())
+
+function allNewDice() {
+  let allNewDiceArray = []
+  for(let i = 0; i < 10; i++) {
+    allNewDiceArray.push({
+      value: Math.floor(Math.random() * 6) + 1,
+      isHeld: false,
+      id: nanoid()
+    })
+  }
+  console.log(allNewDiceArray)
+  return allNewDiceArray
+}
+
+function rollDice() {
+  setDice(allNewDice())
+}
+
+const diceElements = dice.map((die, i) => {
+  return (
+    <Die key={die.id} value={die.value}/>
+  )
+})
+
   return (
     <div>
-      
+      <main>
+        <div className="die-container">
+          {diceElements}
+        </div>
+        <button className="roll-dice" onClick={rollDice}>Roll Dice</button>
+      </main>
     </div>
   )
 }
